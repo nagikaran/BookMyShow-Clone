d399040ebb7cdb92614141e8457f36320dd2fbe8
nagikaran <nagikaran1111@gmail.com>
2023-07-31 13:40:23 +0530

    first commit of the project

---
+++ src/test/java/edu/bookmyshow/project/BookMyShowCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bookmyshow.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
